85435de32f667fe05e07463adf1e8f4a087734f3
man4korea <man4korea@gmail.com>
2025-04-21 10:29:44 +0900

    auto: 작업 저장_2504211029

--- deploy.bat
+++ deploy.bat
@@ -5,19 +5,25 @@
 echo === CorpEasy 배포 스크립트 ===
 
 :: 프론트엔드 빌드
-echo [1/4] 프론트엔드 빌드 중...
+echo [1/5] 프론트엔드 빌드 중...
 cd frontend
 call npm run build
 cd ..
 
 :: 백엔드 함수 준비
-echo [2/4] 백엔드 함수 준비 중...
-if not exist backend\functions\lib mkdir backend\functions\lib
-xcopy /E /I /Y backend\functions\src\*.* backend\functions\lib\
+echo [2/5] 백엔드 함수 의존성 설치 중...
+cd backend\functions
+call npm install
+cd ..\..
+
+echo [3/5] 백엔드 함수 컴파일 중...
+cd backend\functions
+call npm run build
+cd ..\..
 
 :: Firebase 배포
-echo [3/4] Firebase에 배포 중...
+echo [4/5] Firebase에 배포 중...
 call firebase deploy
 
-echo [4/4] 배포 완료
+echo [5/5] 배포 완료
 echo 배포가 완료되었습니다!
